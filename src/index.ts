f5e4a61df632c8191ab91ae9a091c3de39c6e162
luimuril3 <luimuril3@publicisgroupe.net>
2023-05-12 16:55:49 -0600

    add alfred texts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { createServer } from "http";
-import { Server as SocketIOServer } from "socket.io";
 import dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 import crypto from "crypto";
@@ -9,18 +7,32 @@ import fs from "fs";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
-const server = createServer(app);
-const io = new SocketIOServer(server);
 
-app.use(cors());
+const corsOptions = {
+  origin: ["https://houseofsenses.netlify.app", "http://localhost:8000"],
+  methods: ["GET", "POST"],
+  credentials: true,
+  optionsSuccessStatus: 200  // For legacy browser support
+};
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Socket.io server!");
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Received a ${req.method} request to ${req.url}`);
+  next();
 });
 
-io.on("connection", async (socket) => {
-  console.log("A user connected.");
+app.get("/", (req, res) => {
+  res.send("Welcome to the OpenAI server!");
+});
+
+app.get("/voice-note", async ({ query: {  note } }, res) => {
+  console.log("Received voice note.", note);
 
   const configuration = new Configuration({
     // organization: process.env.OPENAI_ORGANIZATION_ID,
@@ -33,52 +45,45 @@ io.on("connection", async (socket) => {
       role: "system",
       content: "You are functioning as a voice note summarizer. Your role is to process the received content, correct any grammatical errors, and present a clear and concise summary. Your task is to ensure clarity and coherence in all the information that was spoken. Furthermore, you are responsible for presenting everything in a well-structured markdown format.",
     },
+    {
+      role: "user",
+      content: note
+    }
   ];
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected.");
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: messages.map(({ content }) => content),
+    max_tokens: 1024,
+    n: 1,
+    stop: ["User:"],
+    temperature: 0.8,
   });
 
-  socket.on("voice note", async (content) => {
-    console.log(`Message received: ${content}`);
+  const summarizedVoiceNote = completion.data.choices[0].text;
+  console.log(`ChatGPT: ${summarizedVoiceNote}`);
 
-    messages.push({ role: "user", content });
-
-    const completion = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: messages,
-      max_tokens: 1024,
-      n: 1,
-      stop: ["User:"],
-      temperature: 0.8,
-    });
-
-    const summarizedVoiceNote = completion.data.choices[0].text;
-    console.log(`ChatGPT: ${summarizedVoiceNote}`);
-    messages.push({
-      role: "assistant",
-      content: summarizedVoiceNote as string,
-    });
-
-    io.emit("summarized voice note", summarizedVoiceNote);
-
-    // save voice note file with fs
-    const hash = crypto
-      .createHash("sha256")
-      .update(summarizedVoiceNote as string)
-      .digest("base64");
-
-    const notesDirectory = '../../notes';
-
-    if (!fs.existsSync(notesDirectory)){
-      fs.mkdirSync(notesDirectory);
-    }
-    fs.writeFileSync(`${notesDirectory}/${hash}.txt`, summarizedVoiceNote as string);
+  messages.push({
+    role: "assistant",
+    content: summarizedVoiceNote as string,
   });
+
+  // save voice note file with fs
+  const hash = crypto
+    .createHash("sha256")
+    .update(summarizedVoiceNote as string)
+    .digest("base64");
+
+  const notesDirectory = '../notes';
+
+  if (!fs.existsSync(notesDirectory)){
+    fs.mkdirSync(notesDirectory);
+  }
+  fs.writeFileSync(`${notesDirectory}/${hash}.txt`, summarizedVoiceNote as string);
+
+  res.json({ message: "Voice note processed.", summarizedVoiceNote });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
